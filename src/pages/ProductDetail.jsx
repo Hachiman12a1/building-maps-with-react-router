2a3ef5bb3d7f017243baee1d8343e1fafbf857f9
Dao Van Dat <daovandat2k15@gmail.com>
2022-08-22 11:25:11 +0700

    273. Extracting Route Params

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 ProductDetail.propTypes = {
     
 };
 
 function ProductDetail(props) {
+    const params = useParams();
+
     return (
         <div>
             <h1>The Product Detail</h1>
+            <p>{params.productId}</p>
         </div>
     );
 }
